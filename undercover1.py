f3965d00e9372ac5876f8011104da15453a0f8f2
nojohnsfirewall <46266130+nojohnsfirewall@users.noreply.github.com>
2019-01-04 02:13:23 -0600

    Add files via upload
    breakthrough

---
+++ undercover1.py
@@ -0,0 +1,32 @@
+#this is a silly text game
+
+print('U n d e r c o v e r : R P G ')
+playerName = input('To start, enter your name, Agent.')
+output_text= 'Good luck, Agent {}.'.format(playerName)
+print(output_text)
+input('Press Enter to Continue')
+
+print('You approach the enemy base.')
+print('Your mission is to infiltrate, and assassinate the military officer.')
+input('(Press Enter to Continue)')
+
+print('If the element of surprise is lost, you will prematurely retire.')
+input('(Press Enter to Continue)')
+
+print('Make your decisions with A or B')
+input('(Press Enter to Continue)')
+
+print('Mission START')
+input('(Press Enter to continue)')
+
+print('Foreign Soil, 1984.\nThere are a couple of different ways to approach the compound.')
+choice=None
+while choice== None:
+    choice = input('Which do you choose?\nA)The the dark cold channel into the sewers.\nB)Advance to the guardpost ahead ')
+    if choice == 'A':
+        print('You slip into your wet suit, and power on your SeaGlide.')
+    elif choice == 'B':
+        print('You get closer to the guard post...\nYou peer at the guard through your binoculars.')
+    else:
+        input:('Please choose with \'A\' or \'B\' ')
+        choice=None
